aded9b58d4b75eaee2ae03046befa96c4d62661a
jennl97 <jennhott97@gmail.com>
2016-09-06 16:53:23 -0500

    First upload of Constants and Variables

---
+++ Contents.swift
@@ -0,0 +1,50 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Assignment
+
+//Variables Explicit (cuz I can)
+
+var myFirstInt:Int = 23
+
+var myFirstFloat:Float = 3.14
+
+var myFirstDouble:Double = 88.33
+
+//Variables Loosely typed
+var mySecondInt = 42
+
+var mySecondFloat = 3.15
+
+var mySecondDouble = 499.236
+
+//Constants
+
+let myFirstConstant = 33
+
+let mySecondConstant = 3.1254
+
+let myThirdConstant = 99.99
+
+//Constants Explicit
+
+let s:String = "Look Ma, No Hands!"
+
+let f:Float = 3.145
+
+let d:Double = 99.987
+
+//Change value of a constant? I don't think so!
+
+let no = 22
+let no = 42
+
+//Float and a 4
+
+let four:Float = 4
+
+/*Well, as you can see, when I assigned a whole number to the data type Float, nothing happened! If I want to use a whole number, I am better off using Int data types */
+
